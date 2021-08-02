fe6ca024d73e732a95f4ad4a436718c0cc1a3bfd
Hector Rangel <hector.rangeljr@yahoo.com>
2021-08-01 19:02:22 -0500

    fin

--- server.js
+++ server.js
@@ -16,10 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI || 'mongodb://localhost/budget_tracker', {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+  useFindAndModify: false
 });
 
 // routes
